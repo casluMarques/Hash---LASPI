b1f9899a3a0929b39f9f44c14cf8d6f6fd5d1f39
casluMarques <131936376+casluMarques@users.noreply.github.com>
2023-04-27 16:57:22 -0300

    Add files via upload

--- hashzada.py
+++ hashzada.py
@@ -1,18 +1,29 @@
 from flask import Flask, request
 import hashlib
 
+def sha256sum(filename):
+    h  = hashlib.sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    data = filename.read()
+    h.update(data[:-1])
+    return h.hexdigest()
+
 app = Flask(__name__)
 
 @app.route("/" , methods=['POST','GET'])
 def form_laspi():
     if request.method == 'POST':
-            arquivo = request.files["file"]
-            ler_arquivo = arquivo.read()
-            sha256hash = hashlib.sha256(ler_arquivo)
-            sha256hashed = sha256hash.hexdigest()
-            return '<h1> {} <h1> '.format(sha256hashed)
+        result =[]
+        arquivos = request.files["file"]
+        print(arquivos)
+        '''for arquivo in arquivos:
+                result.append(sha256sum(arquivo))
+        print(result)
+        '''
+        return '<h1> {} <h1> '.format(hash for hash in result)
     return'''<h1> Deposite toda a documentação enviada: <h1> 
     <form method = 'POST' action = "" enctype = "multipart/form-data">
-            <p><input type = "file" name = "file"><p>
+            <p><input type = "file" name = "file" multiple ><p>
             <p><input type = "submit"><p>
             </form> '''
