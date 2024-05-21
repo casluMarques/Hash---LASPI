9a23ac916b333e9901aa9c231dd23869be55ce6d
casluMarques <131936376+casluMarques@users.noreply.github.com>
2024-05-21 19:12:58 -0300

    Update hashzada.py

--- hashzada.py
+++ hashzada.py
@@ -1,29 +1,44 @@
-from flask import Flask, request
+import os
 import hashlib
 
-def sha256sum(filename):
-    h  = hashlib.sha256()
-    b  = bytearray(128*1024)
-    mv = memoryview(b)
-    data = filename.read()
-    h.update(data[:-1])
-    return h.hexdigest()
+local_txt_hashes = input('local para o txt dos hashes: ')
 
-app = Flask(__name__)
+def calcular_hash_arquivo(caminho_arquivo, algoritmo='sha256'):
+    # Cria o objeto hash usando o algoritmo especificado
+    hash_obj = hashlib.new(algoritmo)
+    
+    # Lê o arquivo em blocos para evitar usar muita memória
+    with open(caminho_arquivo, 'rb') as arquivo:
+        for bloco in iter(lambda: arquivo.read(4096), b''):
+            hash_obj.update(bloco)
+    
+    # Retorna o hash no formato hexadecimal
+    return hash_obj.hexdigest()
 
-@app.route("/" , methods=['POST','GET'])
-def form_laspi():
-    if request.method == 'POST':
-        result =[]
-        arquivos = request.files["file"]
-        print(arquivos)
-        '''for arquivo in arquivos:
-                result.append(sha256sum(arquivo))
-        print(result)
-        '''
-        return '<h1> {} <h1> '.format(hash for hash in result)
-    return'''<h1> Deposite toda a documentação enviada: <h1> 
-    <form method = 'POST' action = "" enctype = "multipart/form-data">
-            <p><input type = "file" name = "file" multiple ><p>
-            <p><input type = "submit"><p>
-            </form> '''
+def percorrer_pasta_e_calcular_hashes(caminho_pasta):
+    #dicionário para associar cada arquivo a seu respectivo hash
+    hashes = {}
+    #andando pelo diretório, calculando hashes e salvando no dicionário
+    for raiz, _, arquivos in os.walk(caminho_pasta):
+        for nome_arquivo in arquivos:
+            caminho_arquivo = os.path.join(raiz, nome_arquivo)
+            hash_arquivo = calcular_hash_arquivo(caminho_arquivo)
+            hashes[caminho_arquivo] = hash_arquivo
+            #print(f'{caminho_arquivo}: {hash_arquivo}')
+    return hashes
+
+# criando o arquivo de hashes
+caminho_da_pasta = input('Path documentação: ')
+nome_texto = input('Nome do arquivo de hashes: ')
+hashes_dos_arquivos = percorrer_pasta_e_calcular_hashes(caminho_da_pasta)
+
+texto = '{}/{}.txt'.format(local_txt_hashes, nome_texto)
+str_hashes = str(hashes_dos_arquivos)
+
+with open(texto, 'w') as escrita:
+    escrita.write(str_hashes)
+
+#hash para o relatório
+
+print('Hash para o relatório: {}'.format(calcular_hash_arquivo(texto)))
+
